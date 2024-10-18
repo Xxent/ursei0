ebbf9396d865e302fc1724fd90fdfb4aa34e68fb
Xxent Diana <missdiasa02@gmail.com>
2024-10-18 10:40:54 +0000

    12

--- src/lib/Comp1.svelte
+++ src/lib/Comp1.svelte
@@ -1,20 +1,29 @@
 <script>
-  export let app_function
+  export let app_function 
+  // передается в этот компонент извне. Содержит функцию, которая будет вызвана в этом компоненте, при нажатии на кнопку.
+  import Fa from 'svelte-fa'
+  // отображения иконок Font Awesome
+  import { faFlag } from '@fortawesome/free-solid-svg-icons'
+  import { faGithub } from '@fortawesome/free-brands-svg-icons';
 </script>
 
 <div class='wrapper'>
-   <!-- в wrapper добавляем текст Component 1 -->
+   <!-- добавление иконок  -->
+   <Fa icon={faFlag} />
+   <Fa icon={faGithub} />
 <h1>
   Component 1
+  <!-- Заголовок 1 уровня --> 
 </h1>
 <div>
   <button on:click={()=>app_function('Сообщение от Comp1')}>Кнопка 1</button>
+  <!-- Кнопка вызывает  функцию app_function  и передает сообщение-->
 </div>
 </div>
 
 <style>
   button{
-      width: 100px;
+      width: 100px; 
       height:50px;
       background-color: purple;
       color:azure
